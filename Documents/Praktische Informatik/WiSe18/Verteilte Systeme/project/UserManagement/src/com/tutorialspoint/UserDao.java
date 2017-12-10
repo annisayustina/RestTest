5f50c52373a1214040ee5a617d6e122931c1ef9f
Annisa Yustina <i.charitable@yahoo.com>
2017-12-10 18:48:32 +0100

    Test commit

--- Documents/Praktische Informatik/WiSe18/Verteilte Systeme/project/UserManagement/src/com/tutorialspoint/UserDao.java
+++ Documents/Praktische Informatik/WiSe18/Verteilte Systeme/project/UserManagement/src/com/tutorialspoint/UserDao.java
@@ -9,7 +9,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream; 
 import java.util.ArrayList; 
 import java.util.List;  
-/*test*/
+/*test code*/
 public class UserDao { 
    public List<User> getAllUsers(){ 
       
